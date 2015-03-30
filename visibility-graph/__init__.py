ffab3dd16f9070463501b08885014587b9a1170b
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-03-29 19:52:00 -0600

    must rename

---
+++ visibility-graph/__init__.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+import networkx as nx
+
+def visibility_graph(series):
+
+    g = nx.Graph()
+    
+    # convert list of magnitudes into list of tuples that hold the index
+    tseries = []
+    n = 0
+    for magnitude in series:
+        tseries.append( (n, magnitude ) )
+        n += 1
+
+    # contiguous time points always have visibility
+    for n in range(0,len(tseries)-1):
+        (ta, ya) = tseries[n]
+        (tb, yb) = tseries[n+1]
+        g.add_node(ta, mag=ya)
+        g.add_node(tb, mag=yb)
+        g.add_edge(ta, tb)
+
+    for a,b in combinations(tseries, 2):
+        # two points, maybe connect
+        (ta, ya) = a
+        (tb, yb) = b
+
+        # let's see all other points in the series
+        for tc, yc in tseries:
+            # other points, not a or b
+            if tc != ta and tc != tb:
+                # does c not obstruct?
+                if yc < yb + (ya - yb) * ( (tb - tc) / (tb - ta) ):
+                    g.add_edge(ta, tb)
+
+
+    return g
