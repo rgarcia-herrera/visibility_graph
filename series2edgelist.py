7ec081fd6a63fc5fd3172d02daff90d9b7091d60
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-03-29 20:02:52 -0600

    use module print out edgelist

--- series2edgelist.py
+++ series2edgelist.py
@@ -1,32 +1,15 @@
-from itertools import combinations
 import fileinput
+from visibility_graph import visibility_graph
+import sys
+from networkx import write_edgelist
 
-# convert list of magnitudes into list of tuples that hold the index
+# read time series from file given or from STDIN
 series = []
-n = 0
 for line in fileinput.input():
-    series.append( (n, float(line.strip()) ) )
-    n += 1
+    series.append( float(line.strip()) )
 
-# contiguous time points always have visibility
-for n in range(0,len(series)-1):
-    (ta, ya) = series[n]
-    (tb, yb) = series[n+1]
-    print "connect ", ta,tb
+# create graph
+g = visibility_graph(series)
 
-for a,b in combinations(series, 2):
-    # two points, maybe connect
-    (ta, ya) = a
-    (tb, yb) = b
-
-    # let's see all other points in the series
-    for tc, yc in series:
-        # other points, not a or b
-        if tc != ta and tc != tb:
-            # does c not obstruct?
-            if yc < yb + (ya - yb) * ( (tb - tc) / (tb - ta) ):
-                print "connect ", ta,tb
-
-
-
-
+# print out edgelist
+write_edgelist(g, sys.stdout, data=False)
